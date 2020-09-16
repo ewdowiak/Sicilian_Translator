4b71160a57a2c0e1f17618a362ef0c1cca6ca0ff
Eryk Wdowiak <eryk@wdowiak.me>
2020-09-15 21:48:06 -0400

    added license to top of all scripts

--- cgi-bin/darreri.pl
+++ cgi-bin/darreri.pl
@@ -1,9 +1,20 @@
 #!/usr/bin/perl
 
-##  Perl script to call machine translator
+##  Copyright 2019 Eryk Wdowiak
+##  
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##  
+##      http://www.apache.org/licenses/LICENSE-2.0
+##  
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
 
-##  Eryk Wdowiak
-##  21 Feb 2020
+##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  ##  
 
 use strict;
 use warnings;
